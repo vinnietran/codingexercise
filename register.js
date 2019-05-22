39c5abb0328268ba8a7789c42d76b704a9726fe2
VinTran <tranquillo.vinnie@gmail.com>
2019-05-22 19:15:41 -0400

    Finished randomizing change

--- register.js
+++ register.js
@@ -1,29 +1,9 @@
-
-var axios = require("axios");
 var fs = require("fs");
 
-// var express = require("express");
-
-// var app = express();
-// var PORT = 3000;
-
-// // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
-
-//   app.get("/", function(req, res) {
-//     res.send("Welcome to the Star Wars Page!");
-//   });
-// $(document).ready(function () {
-//    
-// });
-
+//calling the getvalue function which also calls the makeChange function
 getValue();
 
+//function to read the values in the price.txt file
 function getValue() {
     fs.readFile("price.txt", "utf8", function (error, data) {
         if (error) {
@@ -36,7 +16,8 @@ function getValue() {
             //console.log(prices);
             var rawChange = prices[1] - prices[0];
             var change = rawChange.toFixed(2);
-            //console.log(change);
+
+            //calling the make change function for each value pair in the arracy from the file to compute the change 
             makeChange(change);
         }
     })
@@ -44,42 +25,97 @@ function getValue() {
 
 function makeChange(change) {
     //creating variable for the 'twist the customer asked for so that it can be easily changed if needed
-    var twist = 3; 
+    var twist = 3;
+
     //change will not be needed if the buyer pays the exact amount 
     if (change == 0.00) {
+
+        //logging the result to the console 
         console.log(change + " || " + " NO CHANGE NEEDED" +
             "\n" + "*********************************************************************************************************");
     }
 
     //condition for when the change due is not divisible by 3 
     else if (change % twist != 0) {
-        var cents = change * 100; // 1.68 = 168
-        var totalDollar = (cents / 100) >> 0; //1   
-        var remaining = change - totalDollar; // .68 
-        var totalQuarter = (remaining / .25) >> 0; // 2 
-        var lessQuarter = remaining - (totalQuarter * .25) //.18
-        var totalDime = (lessQuarter / .10) >> 0; // 1 
-        var lessDime = lessQuarter - (totalDime * .10) // .08
-        var totalNickel = (lessDime / .05) >> 0; // 1
-        var lessNickel = lessDime - (totalNickel * .05) // .03
-        var totalPenny = (lessNickel / .01).toFixed(0); //.03
-
-        // try to create conditional statements to only display change that != 0 ie if totalquarter = 0 then dont print it 
-
-        console.log(change + " || " + totalDollar + " dollar(s) " + totalQuarter + " Quarter(s) " + totalDime + " Dime(s) "
-            + totalNickel + " Nickel(s) " + totalPenny + " Pennie(s) " +
-            "\n" + "*********************************************************************************************************");
-
-            // if (totalDollar != 0) {
-            //     $("#tend").append(totalDollar + " Dollar(s) ")
-            // }
+        notTwist(change);
     }
 
     //condition for when the change due is divisible by 3
     else if (change % twist == 0) {
-        var changes = [1, .25, .10, .05, .01]
-        var randChange = changes[Math.floor(Math.random() * changes.length)];
-        console.log(change + "||" + randChange + "\n" + 
-        "*********************************************************************************************************");
+        execute(change);
     }
+}
+
+//function to be called by if statement when change is not divisible by 3 or the client 'twist'
+function notTwist(change) {
+
+    var cents = change * 100;
+    var totalDollar = (cents / 100) >> 0;
+    var remaining = change - totalDollar; 
+    var totalQuarter = (remaining / .25) >> 0;
+    var lessQuarter = remaining - (totalQuarter * .25);
+    var totalDime = (lessQuarter / .10) >> 0;
+    var lessDime = lessQuarter - (totalDime * .10);
+    var totalNickel = (lessDime / .05) >> 0; 
+    var lessNickel = lessDime - (totalNickel * .05);
+    var totalPenny = (lessNickel / .01).toFixed(0); 
+
+    //logging the result to the console 
+    console.log(change + " || " + totalDollar + " dollar(s) " + totalQuarter + " Quarter(s) " + totalDime + " Dime(s) "
+        + totalNickel + " Nickel(s) " + totalPenny + " Pennie(s) " +
+        "\n" + "*********************************************************************************************************");
+}
+
+// The below code block is how I worked around not being able to figure out how to efficiently randomize the change that is generated when the change is 
+//divisible by 3. I very much understand that this is not the most efficient way to do things, and does not even entirely solve the client request
+//None the less, execute() will nonetheless generate a random function call that will either make the change in all dollars, quarters, etc. 
+
+function execute(change) {
+    var i = random();
+    eval('rand' + i + '(change)');
+}
+function random() {
+    var i = Math.floor(Math.random() * 20) % 6;
+    if (i <= 0) return random();
+    return i;
+}
+
+//function for making change in quarters
+function rand1(change) {
+    var totalQuarter = (change / .25) >> 0;
+    //logging the result to the console 
+    console.log(change + " || " + totalQuarter + " Quarter(s) " +
+        "\n" + "*********************************************************************************************************");
+}
+
+//function for making change in nickels 
+function rand2(change) {
+    var totalNickel = (change / .05) >> 0;
+    //logging the result to the console 
+    console.log(change + " || " + totalNickel + " Nickel(s) " +
+        "\n" + "*********************************************************************************************************");
+}
+
+//function for making change in dimes 
+function rand3(change) {
+    var totalDime = (change / .10) >> 0;
+    //logging the result to the console 
+    console.log(change + " || " + totalDime + " Dime(s) " +
+        "\n" + "*********************************************************************************************************");
+}
+
+//function for making change in pennies 
+function rand4(change) {
+    var totalPennie = (change / .01) >> 0;
+    //logging the result to the console 
+    console.log(change + " || " + totalPennie + " Pennie(s) " +
+        "\n" + "*********************************************************************************************************");
+}
+
+//function for making change in dollars
+function rand5(change) {
+    var totalDollar = (change / 1) >> 0;
+    //logging the result to the console 
+    console.log(change + " || " + totalDollar + " Dollar(s) " +
+        "\n" + "*********************************************************************************************************");
 }
